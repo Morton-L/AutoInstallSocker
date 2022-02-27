4961ace7dbd5c12ea8daf77ee42cfeaa6503bdc8
沫延 <Morton.L@Outlook.com>
2022-02-27 11:43:06 +0800

    优化代码

--- SocketAutoInstalling.sh
+++ SocketAutoInstalling.sh
@@ -273,52 +273,9 @@ EOF
 
 # 编译安装OpenSSL
 function InstallOpenSSL(){
-    green " =================================================="
-	green " 开始安装OpenSSL"
-	green " =================================================="
-	sleep 6s
-	# $osSystemPackage remove -y openssl
-	cd /usr/local
-	green " =================================================="
-	green " 开始下载OpenSSL源码..."
-	green " =================================================="
-	wget https://www.openssl.org/source/openssl-1.1.1m.tar.gz
-	# 判断执行结果
-	if [ $? -ne 0 ]; then
-		ErrorInfo=" 下载失败...请检查网络连接"
-		Error
-	fi
-	tar -xvzf openssl-1.1.1m.tar.gz
-	cd openssl-1.1.1m
-	./Configure
-	./config
-	green " =================================================="
-	green " 开始编译并安装OpenSSL..."
-	green " =================================================="
-	make
-	# 判断执行结果
-	if [ $? -ne 0 ]; then
-		ErrorInfo=" 编译失败...请查看日志"
-		Error
-	fi
-	make test
-	# 判断执行结果
-	if [ $? -ne 0 ]; then
-		ErrorInfo=" 编译测试未通过...请重试"
-		Error
-	fi
-	make install
-	# 判断执行结果
-	if [ $? -ne 0 ]; then
-		ErrorInfo=" 安装失败...请查看日志"
-		Error
-	fi
-	mv /usr/bin/openssl /usr/bin/openssl-old
-	ln -s /usr/local/bin/openssl /usr/bin/openssl
-	OpenSSLVersion=$(expr substr "$(/usr/local/bin/openssl version)" 9 6)
-	green " =================================================="
-	green " OpenSSL版本:   ${OpenSSLVersion}"
-	green " =================================================="
+	curl -Oks https://raw.githubusercontent.com/Morton-L/AutoScript_Linux/main/Function/AutoInstallOpenSSL.sh
+	source AutoInstallOpenSSL.sh
+	AutoInstallOpenSSL
 }
 
 # V2ray调整配置
